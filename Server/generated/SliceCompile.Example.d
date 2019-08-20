2d95d0c13367725dfeeff3b7304d0a705b4f8c4a
xlgwr <xlgwr@163.com>
2019-08-20 23:33:21 +0800

    add class

---
+++ Server/generated/SliceCompile.Example.d
@@ -0,0 +1,10 @@
+﻿<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<dependencies>
+  <source name="../Slice/Example.ice">
+    <options>
+      <IceHome>C:\Users\iRobotHP\.nuget\packages\zeroc.ice.net\3.7.2\build\..</IceHome>
+      <IceToolsPath>C:\Users\iRobotHP\.nuget\packages\zeroc.ice.net\3.7.2\build\..\tools</IceToolsPath>
+      <OutputDir>E:\ice\demo.core\Server\generated</OutputDir>
+    </options>
+  </source>
+</dependencies>
